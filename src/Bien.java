aeaac2b64ee86a75bb5cd3fc463c08ed0650c2a2
Alaeddine <ia_nasri@esi.dz>
2019-12-04 00:44:32 +0100

    Tableau des bien + affichage des bien

--- src/Bien.java
+++ src/Bien.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class Bien extends ImmoEsi {
+    private String adresse;
+    private float surface;
+    private float prix;
+    private boolean negociable;
+    private String description,date,photos;
 
-public class Bien {
+    public void afficher(){
+        System.out.println("affiche les details d'un bien");
+    }
+    public void afficher_prix(){
+        System.out.println("prix d'un bien");
+    }
 }
--- src/ImmoEsi.java
+++ src/ImmoEsi.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
-
 public class ImmoEsi {
+    public static int nb_bien; // nombre de bien proposé
+    Bien tab_bien[] = new Bien[nb_bien];
+    Bien tableau_arichive[] ;
+    private int i;
+
+    public ImmoEsi() {
+        // constructeur
+    }
+    public void affi_biens(){
+        i = 0;
+        System.out.println("Le nombre de bien est : "+ nb_bien);
+        for (i=0;i<nb_bien;i++) {
+            System.out.println("Bien numero "+i+" : "+tab_bien[i]);
+        }
+    }
+    public void rechreche(){
+        i = 0;
+        // tableau de criteres
+        for (i=0;i<nb_bien;i++ ) {
+            System.out.println("hna ndirou tableau jdid!");
+        }
+        //
+    }
 }
