2a855db01095ed366c658ce492c1362bedf67019
Alaeddine <ia_nasri@esi.dz>
2019-12-05 01:43:35 +0100

    Class Transaction

---
+++ src/Transactoin.java
@@ -0,0 +1,15 @@
+public class Transac {
+    Transaction_Type transaction;
+
+    public float calcule_prix(Transac transac){
+        float prix = 0;
+        if (transac.transaction == Transaction_Type.Vente ) {
+
+        }
+
+
+        return prix;
+    }
+
+
+}
