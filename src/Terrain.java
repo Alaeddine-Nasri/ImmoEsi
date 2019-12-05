e7e450f1297c671bc8bf8117d6c6999fc10b4276
Alaeddine <ia_nasri@esi.dz>
2019-12-05 01:29:32 +0100

    Const of Class Terrain and Atribues

--- src/Terrain.java
+++ src/Terrain.java
@@ -1,6 +1,10 @@
 public class Terrain extends NonHabitable {
 
 
+    public Terrain(String adresse, float surface, float prix, boolean negociable, String description, String date, String photos, int nb_facades, String etat_juidique) {
+        super(adresse, surface, prix, negociable, description, date, photos, nb_facades, etat_juidique);
+    }
+
     @Override
     public void affich() {
         afficher();
